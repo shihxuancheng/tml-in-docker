062aa7b8fdb9ae764f5c7b7c9ec3a9e82d59de0a
richard <shihxuancheng@gmail.com>
2018-08-31 11:04:40 +0800

    Add extra strup parameters to vm

--- wildfly_base/initial/startwf.sh
+++ wildfly_base/initial/startwf.sh
@@ -1,5 +1,6 @@
 #variables
 IPADDRESS=$(hostname -i)
-/opt/jboss/wildfly/bin/standalone.sh -c ${WF_CONFIG} -b ${IPADDRESS} -Djboss.bind.address.management=${IPADDRESS}
+EXTRA_JAVA_OPTS=$EXTRA_JAVA_OPTS -Djboss.bind.address.management=${IPADDRESS}
+/opt/jboss/wildfly/bin/standalone.sh -c ${WF_CONFIG} -b ${IPADDRESS} ${EXTRA_JAVA_OPTS}
 
 
