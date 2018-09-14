0b1058ec949beb23c199a4a72fc0dcb0f73f9527
richard <shihxuancheng@gmail.com>
2018-09-14 11:35:19 +0800

    allow to add extra runtime java parameters

--- wildfly_base/initial/startwf.sh
+++ wildfly_base/initial/startwf.sh
@@ -1,6 +1,6 @@
 #variables
 IPADDRESS=$(hostname -i)
-EXTRA_JAVA_OPTS="$EXTRA_JAVA_OPTS -Djboss.bind.address.management=${IPADDRESS}"
-/opt/jboss/wildfly/bin/standalone.sh -c ${WF_CONFIG} -b ${IPADDRESS} ${EXTRA_JAVA_OPTS}
+export JAVA_OPTS = "$JAVA_OPTS $EXTRA_JAVA_OPTS"
+/opt/jboss/wildfly/bin/standalone.sh -c ${WF_CONFIG} -b ${IPADDRESS}
 
 
